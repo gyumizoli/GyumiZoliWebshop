277845b155ba67d16a947f4527c059c4e3ce451f
BoBenc <bogerbence2002@gmail.com>
2025-01-31 18:39:03 +0100

    Components és Services hozzáadása

--- GyumiZoli_Frontend/src/app/app.module.ts
+++ GyumiZoli_Frontend/src/app/app.module.ts
@@ -4,10 +4,36 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { FruitsComponent } from './components/fruits/fruits.component';
+import { VegetablesComponent } from './components/vegetables/vegetables.component';
+import { SaleComponent } from './components/sale/sale.component';
+import { ErrorpageComponent } from './components/errorpage/errorpage.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegistrationComponent } from './components/registration/registration.component';
+import { AboutusComponent } from './components/aboutus/aboutus.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ProductDetailComponent } from './components/product-detail/product-detail.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { AdminProfileComponent } from './components/admin-profile/admin-profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    FruitsComponent,
+    VegetablesComponent,
+    SaleComponent,
+    ErrorpageComponent,
+    LoginComponent,
+    RegistrationComponent,
+    AboutusComponent,
+    FooterComponent,
+    ProductDetailComponent,
+    ProfileComponent,
+    AdminProfileComponent
   ],
   imports: [
     BrowserModule,
