9027a1c9ea3d4d8424b79573846ab999179dd99b
Mihucza Gergoő <mihuczagergo@ktch.hu>
2025-02-14 02:39:08 +0100

    Response és Mail Controller hozzáadása

---
+++ GyumiZoli_Backend/app/Http/Controllers/MailController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers\api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Mail\BannerMail;
+use Illuminate\Support\Facades\Mail;
+
+
+class MailController extends Controller
+{
+    public function sendMail($userName,$bannedTime){
+        $content = [
+            "title" => "Felhasználó blokkolása",
+            "user" => $userName,
+            "time"=> $bannedTime
+            
+        ];
+        Mail::to("")-> send(new BannerMail($content));
+
+    }
+    
+}
